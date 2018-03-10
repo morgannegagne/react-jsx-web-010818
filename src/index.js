7fd5379ed77dfa188bec2aa900bff0997bff98a1
morgannegagne <morgannegagne@gmail.com>
2018-03-10 13:28:18 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-// Code goes here
+import React from 'react';
+
+import { message } from './components/foo';
+
+import fruit from './components/fruits';
+console.log(fruit.apple); // prints 'red'
+
+// In another file, also in the same directory
+import { apple } from './components/fruits';
+console.log(apple); // prints 'red'
